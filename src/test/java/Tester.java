5bc435780eb8db53a104021f79521d7844ad49e0
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-25 18:59:44 -0800

    Update Tester.java

--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -13,40 +13,16 @@ public class Tester {
 
    @Test
    public void partA () {
-      int result = APCalendar.numberOfLeapYears(0,2000);
-      assertEquals(486,result);
-      result = APCalendar.numberOfLeapYears(1950,1960);
-      assertEquals(3,result);
-      result = APCalendar.numberOfLeapYears(1900,2020);
-      assertEquals(30,result);
-      result = APCalendar.numberOfLeapYears(2000,2021);
-      assertEquals(6,result);
-   }
-   @Test
-   public void partB(){
-      /*
-      1999 Feb 5 5 Nov 15 1   2000 Feb 5 6 Nov 15 3
-      2001 Feb 5 1 Nov 15 4   2002 Feb 5 2 Nov 15 5
-      2003 Feb 5 3 Nov 15 6   2004 Feb 5 4 Nov 15 1
-      2005 Feb 5 6 Nov 15 2   2006 Feb 5 0 Nov 15 3
-      2007 Feb 5 1 Nov 15 4
-    */
-       int result = APCalendar.dayOfWeek(2,5,1999);
-       assertEquals(5,result);
-       result = APCalendar.dayOfWeek(11,15,1999);
-       assertEquals(1,result);
-       result = APCalendar.dayOfWeek(2,5,2000);
-       assertEquals(6,result);
-       result = APCalendar.dayOfWeek(11,15,2000);
-       assertEquals(3,result);
-       result = APCalendar.dayOfWeek(2,5,2001);
-       assertEquals(1,result);
-       result = APCalendar.dayOfWeek(11,15,2001);
-       assertEquals(4,result);
-       result = APCalendar.dayOfWeek(2,5,2002);
-       assertEquals(2,result);
-       result = APCalendar.dayOfWeek(11,15,2002);
-       assertEquals(5,result);
+      APLine line1 = new APLine(5,4,-17);
+      double slope1 = line1.getSlope();
+      assertEquals(slope1,-1.25,0.01);
+      boolean onLine1 = line1.isOnLine(5,-2);
+      assertEquals(onLine1,true);
+      APLine line2 = new APLine(-25,40,30);
+      double slope2 = line2.getSlope();
+      assertEquals(slope2,0.625,0.01);
+      boolean onLine2 = line2.isOnLine(5,-2);
+      assertEquals(onLine2,false);
    }
 
 
