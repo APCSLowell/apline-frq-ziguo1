0fb250c375f4b0827c44cd19bedbbf78fa1f76c6
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-25 18:58:41 -0800

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,13 @@
 public class Main {
-  public static void main(String[] args)
-  {
-    for (int year = 1999; year <= 2007; year++)
-      System.out.println(year + " Feb 5 " + APCalendar.dayOfWeek(2, 5, year) +
-                                " Nov 15 " + APCalendar.dayOfWeek(11, 15, year));
+  public static void main(String[] args) {
+    APLine line1 = new APLine(5, 4, -17);
+    double slope1 = line1.getSlope();
+    boolean onLine1 = line1.isOnLine(5, -2);
+    System.out.println(slope1 + " " + onLine1);
 
-    /*
-      1999 Feb 5 5 Nov 15 1
-      2000 Feb 5 6 Nov 15 3
-      2001 Feb 5 1 Nov 15 4
-      2002 Feb 5 2 Nov 15 5
-      2003 Feb 5 3 Nov 15 6
-      2004 Feb 5 4 Nov 15 1
-      2005 Feb 5 6 Nov 15 2
-      2006 Feb 5 0 Nov 15 3
-      2007 Feb 5 1 Nov 15 4
-    */
+    APLine line2 = new APLine(-25, 40, 30);
+    double slope2 = line2.getSlope();
+    boolean onLine2 = line2.isOnLine(5, -2);
+    System.out.println(slope2 + " " + onLine2);
   }
 }
